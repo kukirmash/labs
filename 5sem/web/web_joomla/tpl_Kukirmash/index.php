500e98fe92e8b077315579760e5f7afa5ccc593f
kukirmash <ivanya-2000@mail.ru>
2025-11-23 23:50:47 +0300

    WEB lab 2

--- 5sem/web/web_joomla/tpl_Kukirmash/index.php
+++ 5sem/web/web_joomla/tpl_Kukirmash/index.php
@@ -51,10 +51,9 @@ $sitename = htmlentities($app->get('sitename'), ENT_QUOTES, 'UTF-8');
 <body id="body">
 	<div class="container" id="body-container">
 
-
 		<!-- =================== HEADER =================== -->
-		<header id="header" class="py-3">
-			<div class="row align-items-center">
+		<header>
+			<div class="row" id="header" >
 
 				<!-- Логотип (3 колонки) -->
 				<div class="col-3" id="header-logo">
@@ -64,96 +63,69 @@ $sitename = htmlentities($app->get('sitename'), ENT_QUOTES, 'UTF-8');
 						<h1 id="logo-text"><?php echo $sitename; ?></h1>
 					<?php endif; ?>
 				</div>
-				
-				<!-- Поиск (6 колонки) -->
-				<div class="col-6" id="header-logo">
 					
-				</div>
-
-
-				<!-- Поиск (8 колонок) -->
-				<div class="col-8" id="header-search">
-					<?php if ($this->countModules('kukirmash-search')) : ?>
-						<jdoc:include type="modules" name="kukirmash-search" />
-					<?php endif; ?>
-				</div>
-
+				<!-- Позиция header -->
+				<?php if ($this->countModules('kukirmash-header')) : ?>
+					<jdoc:include type="modules" name="kukirmash-header" />
+				<?php endif; ?>
 
 			</div>
 		</header>
+		
+		<!-- =================== МЕНЮ =================== -->
+		<div class="row">
+			<section class="col-12" id="topmenu">
+				<?php if ($this->countModules('kukirmash-topmenu')) : ?>
+					<jdoc:include type="modules" name="kukirmash-topmenu" />
+				<?php endif; ?>
+			</section>
+		</div>
 
-
-
-
-		<!-- =================== ОСНОВНАЯ СЕТКА =================== -->
-		<main id="main" class="mt-4">
-			<div class="row" id="main-row">
-
-
+		<!-- =================== ОСНОВНОЕ СОДЕРЖИМОЕ =================== -->
+		<main>
+			<div class="row">
 				<!-- Левая колонка (3/12) -->
-				<aside class="col-md-3" id="left-sidebar">
+				<aside class="col-3" id="left-sidebar">
 					<?php if ($this->countModules('kukirmash-left')) : ?>
 						<jdoc:include type="modules" name="kukirmash-left" />
 					<?php endif; ?>
 				</aside>
 
-
 				<!-- Основное содержимое (6/12) -->
-				<section class="col-md-6" id="main-content">
+				<section class="col-6" id="main-content">
 					<jdoc:include type="component" />
 				</section>
 
-
 				<!-- Правая колонка (3/12) -->
-				<aside class="col-md-3" id="right-sidebar">
+				<aside class="col-3" id="right-sidebar">
 					<?php if ($this->countModules('kukirmash-right')) : ?>
 						<jdoc:include type="modules" name="kukirmash-right" />
 					<?php endif; ?>
 				</aside>
 
-
 			</div>
 		</main>
 
+		<!-- =============== БЛОК ДЛЯ МОДУЛЯ =============== -->
+		<div class="row">
+			<section class="col-12" id="module">
+				<?php if ($this->countModules('kukirmash-module')) : ?>
+					<jdoc:include type="modules" name="kukirmash-module" />
+				<?php endif; ?>
+			</section>
+		</div>
+		
+		<!-- =============== ПОДВАЛ =============== -->
+		<div class="row">
+			<section class="col-12" id="footer">
+				<p>Подвал</p>
+				<?php if ($this->countModules('kukirmash-footer')) : ?>
+					<jdoc:include type="modules" name="kukirmash-footer" />
+				<?php endif; ?>
+			</section>
+		</div>
 
-
-
-		<!-- =============== БЛОК ПОД ОСНОВНЫМ КОНТЕНТОМ =============== -->
-		<section id="module" class="mt-4">
-			<?php if ($this->countModules('kukirmash-module')) : ?>
-				<jdoc:include type="modules" name="kukirmash-module" />
-			<?php endif; ?>
-		</section>
-
-
-
-
-		<!-- =================== FOOTER =================== -->
-		<footer id="footer" class="py-4 mt-5 border-top">
-			<div class="row" id="footer-row">
-
-
-				<div class="col-md-4" id="footer-left">
-					<?php if ($this->countModules('kukirmash-footer')) : ?>
-						<jdoc:include type="modules" name="kukirmash-footer" />
-					<?php endif; ?>
-				</div>
-
-
-				<div class="col-md-4 text-center" id="footer-center">
-					<small><?php echo $sitename; ?> © <?php echo date('Y'); ?></small>
-				</div>
-
-
-				<div class="col-md-4" id="footer-right">
-					<jdoc:include type="message" />
-				</div>
-
-
-			</div>
-		</footer>
-
-
+		<!-- =============== =============== =============== -->
 	</div>
 </body>
 
